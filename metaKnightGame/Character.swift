82c964dc1c71863fe55e1ca716f195015566c9bf
Vu Dang <jublusion5@gmail.com>
2015-12-07 00:48:04 -0600

    started Character class

---
+++ metaKnightGame/Character.swift
@@ -0,0 +1,16 @@
+//
+//  Character.swift
+//  metaKnightGame
+//
+//  Created by Vu Dang on 12/7/15.
+//  Copyright © 2015 Vu Dang. All rights reserved.
+//
+
+import Foundation
+
+class Character
+{
+    var _hp: Int = 100
+    var _atkPwr: Int = 60
+
+}
